9ee10541972f99af0fe19440cd4b436d26aefa4b
ronivay <roni@vayrynen.info>
2020-05-21 21:26:12 +0300

    change infinite loop in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 function StopProcesses {
 
 	while [ $(/usr/bin/monit status | sed -n '/^Process/{n;p;}' | awk '{print $2}' | grep -c OK) != 0 ] ; do
-		sleep 2
+		sleep 1
 		/usr/bin/monit stop all
 	done
 
@@ -17,4 +17,9 @@ trap StopProcesses EXIT TERM
 
 /usr/bin/monit && /usr/bin/monit start all
 
-while true; do : ; done
+while true
+do
+	sleep 1d
+done &
+
+wait $!
